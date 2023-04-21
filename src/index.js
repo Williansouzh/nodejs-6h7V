d13aa26f04327539524a737988ab0d543ceb9df2
Willian Souza <91621589+Williansouzh@users.noreply.github.com>
2023-04-21 10:28:41 -0300

    s

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const Sequelize = require('sequelize');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+
+// Crie uma conexão com o banco de dados MySQL usando Sequelize
+const sequelize = new Sequelize('mysql://root:jGqabMzaBB3GVogy47cl@containers-us-west-206.railway.app:7652/railway', {
+  host: 'localhost',
+  dialect: 'mysql'
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+// Teste a conexão com o banco de dados
+sequelize.authenticate().then(() => {
+  console.log('Conexão bem sucedida!');
+}).catch(error => {
+  console.error('Não foi possível conectar ao banco de dados:', error);
+});
+
+// Rota raiz
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
+
+// Inicie o servidor na porta 3000
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
 });
